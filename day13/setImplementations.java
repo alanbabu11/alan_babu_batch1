0a3e25a1416db2e1328baea4c917c1114408e887
alanbabu11 <alanbabuk21@gmail.com>
2026-04-10 08:23:35 +0530

    day12

---
+++ day13/setImplementations.java
@@ -0,0 +1,24 @@
+package day13;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class setImplementations {
+    public static void main(String[] args) {
+        // Set<Integer> set  = new HashSet<>();
+        // Integer a = 1;
+        // set.add(a);
+        // System.out.println(set);
+        // set.add(0);
+        // System.out.println(set);
+        Set<String> set = new HashSet<>();
+        set.add("KSP");
+        System.out.println(set);
+        set.add("RampeX");
+        System.out.println(set);
+        set.add("Alan");
+        set.add("Akshaya");
+        System.out.println(set);
+
+    }
+}
